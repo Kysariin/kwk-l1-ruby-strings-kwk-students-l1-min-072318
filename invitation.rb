e6b12df6ba26b7e960b486b7d99109d1e16344d2
kate harris <kateharris03@gmail.com>
2018-07-23 19:58:20 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,19 +2,19 @@
 
 # Try starting out with puts'ing a string.
 puts "Hi, you've been invited to a party! What is your name?"
-guest_name = gets.chomp
+guest_name = gets.chomp.gsub(/\w+/, &:capitalize)
 puts "What's the occasion?"
-party_name = gets.chomp
+party_name = gets.chomp.gsub(/\w+/, &:capitalize)
 puts "What date is the party?"
-date = gets.chomp
+date = gets.chomp.gsub(/\w+/, &:capitalize)
 puts "What time is the party?"
-time = gets.chomp
+time = gets.chomp.gsub(/\w+/, &:capitalize)
 puts "Who's hosting?"
-host_name = gets.chomp
+host_name = gets.chomp.gsub(/\w+/, &:capitalize)
 
 puts "Dear #{guest_name},
 
-You are cordially invited to #{party_name} at #{date}. Please RSVP as soon as possible.
+You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP as soon as possible.
 
 Sincerely,
 
