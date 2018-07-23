f01b415d10d3eb3d9eb18ff5621b75cdd73b9eb8
kate harris <kateharris03@gmail.com>
2018-07-23 19:49:51 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -6,4 +6,14 @@ guest_name = gets.chomp.titleize
 puts "What's the occasion?"
 party_name = gets.chomp.titleize
 puts "When is the party?"
-date = gets.chomp
+date = gets.chomp.titleize
+puts "Who's hosting?"
+host_name = gets.chomp.titleize
+
+puts "Dear #{guest_name},
+
+You are cordially invited to #{party_name} at #{date}. Please RSVP as soon as possible.
+
+Sincerely,
+
+#{host_name}"
