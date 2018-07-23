3a04cb6b5e80005b7b693f06927b4f7f19a17465
kate harris <kateharris03@gmail.com>
2018-07-23 19:48:07 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,4 +2,8 @@
 
 # Try starting out with puts'ing a string.
 puts "What's the guest's name?"
-guest_name = gets.chomp.cap
+guest_name = gets.chomp.titleize
+puts "What's the occasion?"
+party_name = gets.chomp.titleize
+puts "When is the party?"
+date = gets.chomp
